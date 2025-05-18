6f388f6ada0a030cd00dd24bfeb7f734abee0bbb
Susann Struwe <Susann.Struwe@gmail.com>
2025-05-18 14:20:54 +0200

    Added webpack types and DefinePlugin to remove warnings in browser

--- projects/angular-component-library/.storybook/main.ts
+++ projects/angular-component-library/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/angular';
+import * as webpack from 'webpack';
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -15,10 +16,30 @@ const config: StorybookConfig = {
     docs: {
         autodocs: true
     },
-    webpackFinal: async (config) => {
+    webpackFinal: async (config, { configType }) => {
+        const mode =
+            typeof configType === 'string'
+                ? configType.toLowerCase()
+                : 'development';
+
         config.performance = {
             hints: false
         };
+        // Remove old DefinePlugin with NODE_ENV
+        config.plugins = config.plugins?.filter(
+            (plugin) =>
+                !(
+                    plugin instanceof webpack.DefinePlugin &&
+                    'process.env.NODE_ENV' in (plugin.definitions || {})
+                )
+        );
+        // Add new DefinePlugin with consistent NODE_ENV
+        config.plugins?.push(
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify(mode)
+            })
+        );
+
         return config;
     },
     staticDirs: ['static']
