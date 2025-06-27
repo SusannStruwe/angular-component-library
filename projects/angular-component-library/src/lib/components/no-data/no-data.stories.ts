df6ba34e6cc14962c4f390c1fcd15fca1243228f
Susann Struwe <susann.struwe@gmail.com>
2025-06-27 15:59:24 +0200

    Added ng content to no-data-component

--- projects/angular-component-library/src/lib/components/no-data/no-data.stories.ts
+++ projects/angular-component-library/src/lib/components/no-data/no-data.stories.ts
@@ -1,6 +1,16 @@
 import { Meta, StoryObj } from '@storybook/angular';
 import { NoDataComponent } from './no-data.component';
 
+
+const defaultTemplateMock = `   <no-data-component>
+        <div content>
+            <p> Inner Content </p>
+        </div>
+    </no-data-component>
+ `;
+
+
+
 const meta: Meta<NoDataComponent> = {
     title: 'Components/No Data',
     component: NoDataComponent,
@@ -17,4 +27,11 @@ export const Sample: Story = {
     args: {
         translationKey: 'data.noData'
     }
+};
+
+
+export const SampleNgContent: Story = {
+    render: () => ({
+        template: defaultTemplateMock
+    })
 };
