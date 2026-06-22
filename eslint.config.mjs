8995979f5d1904d8ec962a019af8c8b03e6a1b90
Susann Struwe <susann.struwe@gmail.com>
2026-06-22 17:26:30 +0200

    Added type, formatted files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,60 +1,60 @@
-import js from "@eslint/js";
-import angular from "angular-eslint";
-import prettier from "eslint-config-prettier/flat";
-import storybook from "eslint-plugin-storybook";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import angular from 'angular-eslint';
+import prettier from 'eslint-config-prettier/flat';
+import storybook from 'eslint-plugin-storybook';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     {
-        ignores: ["coverage/**", "dist/**", "storybook-static/**"],
+        ignores: ['coverage/**', 'dist/**', 'storybook-static/**']
     },
     {
-        files: ["projects/angular-component-library/**/*.ts"],
+        files: ['projects/angular-component-library/**/*.ts'],
         extends: [
             js.configs.recommended,
             ...tseslint.configs.recommended,
             ...angular.configs.tsRecommended,
-            prettier,
+            prettier
         ],
         processor: angular.processInlineTemplates,
         rules: {
-            "@typescript-eslint/no-explicit-any": "off",
-            "@typescript-eslint/no-unused-vars": "off",
-            "@angular-eslint/directive-selector": [
-                "error",
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            '@angular-eslint/directive-selector': [
+                'error',
                 {
-                    type: "attribute",
+                    type: 'attribute',
                     prefix: [],
-                    style: "camelCase",
-                },
+                    style: 'camelCase'
+                }
             ],
-            "@angular-eslint/component-selector": [
-                "error",
+            '@angular-eslint/component-selector': [
+                'error',
                 {
-                    type: "element",
+                    type: 'element',
                     prefix: [],
-                    style: "kebab-case",
-                },
-            ],
-        },
+                    style: 'kebab-case'
+                }
+            ]
+        }
     },
     {
-        files: ["projects/angular-component-library/**/*.html"],
+        files: ['projects/angular-component-library/**/*.html'],
         extends: [
             ...angular.configs.templateRecommended,
             ...angular.configs.templateAccessibility,
-            prettier,
+            prettier
         ],
-        rules: {},
+        rules: {}
     },
     {
-        files: ["projects/angular-component-library/src/lib/stories/**/*.ts"],
+        files: ['projects/angular-component-library/src/lib/stories/**/*.ts'],
         rules: {
-            "@typescript-eslint/consistent-type-imports": [
-                "error",
-                { disallowTypeAnnotations: false },
-            ],
-        },
+            '@typescript-eslint/consistent-type-imports': [
+                'error',
+                { disallowTypeAnnotations: false }
+            ]
+        }
     },
-    ...storybook.configs["flat/recommended"],
+    ...storybook.configs['flat/recommended']
 );
